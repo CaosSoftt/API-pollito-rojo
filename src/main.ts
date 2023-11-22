86bfe4fd18c92670740ff841706b766c0f90ad10
Alanbalza <107290740+Alanbalza@users.noreply.github.com>
2023-11-22 14:21:00 -0600

    {soy gay alan}

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import { config } from "./config";
 import { routes } from "./Producto/infrastructure/RouteUser";
 import "dotenv/config";
 import { rateLimit } from "express-rate-limit";
-
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
+
 
 const accountLimiter = rateLimit({
   windowMs: 10 * 60 * 1000, 
@@ -24,3 +26,16 @@ const { port } = config.server;
 app.listen(port, () => {
   console.log(`[APP] - corriendo puerto ${port}`);
 });
+
+
+https.createServer({
+  cert: fs.readFileSync('/etc/letsencrypt/archive/api.caossoft.com/fullchain1.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/archive/api.caossoft.com/privkey1.pem')
+}, app).listen(port, function () {
+  console.log('Servidor https corriendo en el puerto 443');
+})
+app.get('/', function (req, res) {
+  res.send('Hola, estas en la pagina inicial');
+  console.log('Se recibio una petición get a través de https');
+});
+
